2826420e3f0f9810a5b093335a943963f1b133d5
Luis M. Lopez <luismlopez2004@gmail.com>
2025-07-28 18:05:40 -0400

    Update exerciseFive.m

--- Vectors & Matrices/exerciseFive.m
+++ Vectors & Matrices/exerciseFive.m
@@ -1,2 +1,9 @@
-% This is a temp file for exercise five and below is filler so that github can recognize this file as a matlab file
-disp('This is a MATLAB placeholder file.');
+% Solving a set of linear equations with inv()
+% 
+% 5x + 7y = 10
+% 4x + 2y = 3
+
+A = [5, 7; 4, 2];
+B = [10; 3];
+C = inv(A) * B;
+D = A \ B; % Alternative way to write the line above that may be faster, and less prone to rounding errors
