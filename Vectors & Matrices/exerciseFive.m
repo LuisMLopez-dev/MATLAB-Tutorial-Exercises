1b62eda05b8728f9c90ff7bf814509399de11f88
Luis M. Lopez <luismlopez2004@gmail.com>
2025-07-28 17:36:56 -0400

    Update exerciseFive.m

--- Vectors & Matrices/exerciseFive.m
+++ Vectors & Matrices/exerciseFive.m
@@ -1,4 +1,2 @@
 % This is a temp file for exercise five and below is filler so that github can recognize this file as a matlab file
-x = 2;
-y = 3;
-exy = exp(x * y);
+disp('This is a MATLAB placeholder file.');
