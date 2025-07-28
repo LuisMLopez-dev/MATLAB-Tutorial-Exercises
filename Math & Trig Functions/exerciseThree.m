94e9e68d7eac9b96b9322238af7edc2fd53d976a
Luis M. Lopez <luismlopez2004@gmail.com>
2025-07-28 17:11:47 -0400

    Add files via upload

---
+++ Math & Trig Functions/exerciseThree.m
@@ -0,0 +1,6 @@
+x = 1.2;
+y = 2.3;
+
+V1 = (3 * cos(x) * sin(4 * y) + exp(x * y)) / (log(x^2 * y));
+V2 = 5 * tan(V1) * sin(5 * x * y) * log10(x * V1);
+V3 = (9 * log(x * y * V1)) / (15 * log10(V1 * V2));
